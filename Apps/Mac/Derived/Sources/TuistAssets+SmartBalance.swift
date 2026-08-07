c8d40f528ba3c8b7e434f72f8abb4c5c51610ee9
YancyFeng <yancyfeng@YancyFengdeMacBook-Pro.local>
2026-08-07 17:35:33 +0800

    chore: 同步 Xcode 工程引用与 Tuist AppLogo 资源

--- Apps/Mac/Derived/Sources/TuistAssets+SmartBalance.swift
+++ Apps/Mac/Derived/Sources/TuistAssets+SmartBalance.swift
@@ -31,6 +31,7 @@ import SwiftUI
 // MARK: - Asset Catalogs
 
 public enum SmartBalanceAsset: Sendable {
+  public static let appLogo = SmartBalanceImages(name: "AppLogo")
   public static let menuBarIcon = SmartBalanceImages(name: "MenuBarIcon")
   public static let providerDeepseek = SmartBalanceImages(name: "Provider_deepseek")
   public static let providerDmxapi = SmartBalanceImages(name: "Provider_dmxapi")
